5a57a2aec5cdebf29bba5500fc3d87f67ba7d00f
victorcreativee <tuyizerevictor@gmail.com>
2025-07-01 09:55:52 +0200

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-softBlue text-primary flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to Betta Budget App</h1>
+    </div>
+  );
+}
+
+export default App;
