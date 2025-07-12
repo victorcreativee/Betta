2c71e647a6ee451c978d31359930cc9ceef7fc17
victorcreativee <tuyizerevictor@gmail.com>
2025-07-12 17:58:12 +0200

    frontend

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/user'); // your User model
+const User = require('../models/user'); 
 const jwt = require('jsonwebtoken');
 
 passport.use(
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5050/api",
+  // baseURL: "http://localhost:5050/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
+
 });
 
 export default instance;
