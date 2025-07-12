5d582d10f0d972599f215a2e9f1e5467694b34a7
victorcreativee <tuyizerevictor@gmail.com>
2025-07-12 18:36:18 +0200

    server

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5050/api/users/login", {
+      const res = await fetch('axios.post("/users/login")', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
@@ -36,7 +36,7 @@ export default function Login() {
     try {
       const decoded = jwtDecode(credentialResponse.credential);
 
-      const res = await fetch("http://localhost:5050/api/users/google-login", {
+      const res = await fetch('axios.post("/users/google-login")', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
