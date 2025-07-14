f635e51fe81d121de4bd498b840e8e78d30219e2
victorcreativee <tuyizerevictor@gmail.com>
2025-07-14 15:29:30 +0200

    server error fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
+
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5050/api",
 });
+
+// const instance = axios.create({
+//   // baseURL: process.env.REACT_APP_API_URL + "/api",
+  
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
 export default instance;
